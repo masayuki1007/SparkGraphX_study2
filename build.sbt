fa306df7289b2010f1b4204347aa10f94e09ffd7
unknown <usr0102026@JIND1198.GMO.LOCAL>
2015-05-13 17:15:42 +0900

    first commit

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "Simple Project"
+
+version := "1.0"
+
+scalaVersion := "2.10.4"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.3.1"
+libraryDependencies += "org.apache.spark" % "spark-graphx_2.10" % "1.3.1"
+
---
+++ src/main/scala/sample1.scala
@@ -0,0 +1,22 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.graphx._
+
+// sbt run
+// https://spark.apache.org/docs/latest/graphx-programming-guide.html
+
+object sample1 {
+
+  def main(args: Array[String]): Unit = {
+    // https://spark.apache.org/docs/latest/quick-start.html
+    val conf = new SparkConf().setAppName("Simple Application")
+
+    //https://spark.apache.org/docs/latest/submitting-applications.html#master-urls
+    conf.setMaster("local")
+    val sc = new SparkContext(conf)
+
+    val graph = GraphLoader.edgeListFile(sc, s"data/sample1.tsv")
+    graph.edges.collect.foreach(println(_))
+  }
+}
